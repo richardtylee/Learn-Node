bc749f354b0cca3c38c0d44a52b3f339055d03ec
richardleebr <richardlee@bleacherreport.com>
2017-05-21 13:21:48 -0700

    04 - Core Concept - Routing

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -3,7 +3,17 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  console.log("Hey!!!");
+  const wes = {name: 'Wes', age: 100, cool: true}
+  //res.send('Hey! It works!');
+  //res.json(wes);
+  //res.send(req.query.name);
+  res.json(req.query);
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
