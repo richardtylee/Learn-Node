c70711c9fcc17c321de0777f1e735a48560965d1
Richard Lee <richardlee@bleacherreport.com>
2017-06-02 14:24:35 -0700

    14 - Creating an Editing Flow for Stores

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -6,12 +6,12 @@ exports.homePage = (req, res) => {
 };
 
 exports.addStore = (req, res) => {
-  res.render('editStore', { title: 'Add Store' })
+  res.render('editStore', { title: 'Add Store' });
 };
 
 exports.createStore = async (req, res) => {
   const store = await (new Store(req.body)).save();
-  req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+  req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
   res.redirect(`/store/${store.slug}`);
 };
 
@@ -20,3 +20,25 @@ exports.getStores = async(req, res) => {
   const stores = await Store.find();
   res.render('stores', { title: 'Stores', stores });
 };
+
+exports.editStore = async(req, res) => {
+  // 1. Find the store given the ID
+  const store = await Store.findOne({ _id: req.params.id });
+  // 2. Confirm they are the owner of the store
+  // TODO
+  // 3. Render out the edit form so the user can update their store
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  // 1. Find and update a store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body,
+    {
+    new: true, // return new store instead of the old one
+    runValidators: true
+  }).exec();
+  // 2. Redirect them the store and tell them in worked
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="store/${store.slug}">View Store</a>`);
+  res.redirect(`/stores/${store._id}/edit`);
+};
+
